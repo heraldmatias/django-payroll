9fdf9cc4a5cb5538fb77f007746de61e7f7d6cce
heraldmatias <heraldmatias.oz@gmail.com>
2014-02-07 15:18:05 -0500

    feliz año nuevo

--- src/inei/planilla/ana.py
+++ src/inei/planilla/ana.py
@@ -6,9 +6,10 @@ from django.db import connection
 def update_planilla():
     cr = connection.cursor()
     sql = 'UPDATE planilla_historicas AS p SET codi_tomo = f.codi_tomo, num_folio = f.num_folio FROM folios f WHERE ' \
-          'p.codi_folio = f.codi_folio AND f.codi_tomo=%s;'
-    cr.execute(sql, (41, ))
-    print 'Actualizado'
+          'p.codi_folio = f.codi_folio AND f.codi_tomo=%s AND p.codi_tomo IS NULL;'
+    for tomo in range(42, 420):
+        cr.execute(sql, (tomo, ))
+        print 'Actualizado %s' % tomo
 
 
 update_planilla()
